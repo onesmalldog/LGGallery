aba0f938a59e9fe24b6866d7daa0017b9c679ce9
lee zg <598407521@qq.com>
2016-07-20 19:16:41 +0800

    Create LGCollectionCell.h

---
+++ LGGallery/view/LGCollectionCell.h
@@ -0,0 +1,21 @@
+
+//  Created by 东途 on 16/7/20.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum : NSUInteger {
+    SHOWImage,
+    SHOWVideo
+} SHOW;
+
+@interface LGCollectionCell : UICollectionViewCell
+/** image */
+@property (weak, nonatomic) UIImageView *imageItem;
+/** 视频的时长 during of the video */
+@property (weak, nonatomic) UILabel *during;
+/** 展示图片或者视频，请先设置这个属性 show image or video, set this property first */
+@property (assign, nonatomic) SHOW showWhat;
+/** initlized */
++ (LGCollectionCell *)cellWithCollection:(UICollectionView *)collection indexPath:(NSIndexPath *)indexPath;
+@end
