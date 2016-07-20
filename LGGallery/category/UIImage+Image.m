49b08efcf18f4e78aaebd7b5012ef6079cb58820
lee zg <598407521@qq.com>
2016-07-20 19:02:10 +0800

    Create UIImage+Image.m

---
+++ LGGallery/category/UIImage+Image.m
@@ -0,0 +1,16 @@
+
+//  Created by apple on 15-3-4.
+//
+
+#import "UIImage+Image.h"
+
+@implementation UIImage (Image)
++ (instancetype)imageWithOriginalName:(NSString *)imageName {
+    UIImage *image = [UIImage imageNamed:imageName];
+    return [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+}
++ (instancetype)imageWithStretchableName:(NSString *)imageName {
+    UIImage *image = [UIImage imageNamed:imageName];
+    return [image stretchableImageWithLeftCapWidth:image.size.width * 0.5 topCapHeight:image.size.height * 0.5];
+}
+@end
